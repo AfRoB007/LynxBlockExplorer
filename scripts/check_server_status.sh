943cb1b1aba7ab835ffd37707d06f038489f79ff
Ben <ben@getlynx.io>
2018-05-02 14:39:31 -0400

    Results from Testing (JD01)

--- scripts/check_server_status.sh
+++ scripts/check_server_status.sh
@@ -2,16 +2,16 @@
 
 if [ -f tmp/index.pid ]; then
   echo "file index.pid exists" >>/tmp/index-state.log
-  ls -lt >>/tmp/index-state.log
+  ls -lt tmp >>/tmp/index-state.log
   if [ $(ps fax | grep "sync.js index update" | grep -v grep | wc -l) -gt 1 ]; then
     echo "stop all sync.js processes" >>/tmp/index-state.log
-      #killall nodejs
+      killall nodejs
     echo "remove index.pid" >>/tmp/index-state.log
-      #rm -rf tmp/index.pid
+      rm -rf tmp/index.pid
   fi
   if [ $(ps fax | grep "sync.js index update" | grep -v grep | wc -l) -eq 0 ]; then
     echo "remove index.pid" >>/tmp/index-state.log
       rm -rf tmp/index.pid
   fi
-  ls -lt >>/tmp/index-state.log
+  ls -lt tmp >>/tmp/index-state.log
 fi
