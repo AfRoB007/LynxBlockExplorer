0ec2e1a59302e0a73ed93f0e8f90bc17710713e3
Ben <ben@getlynx.io>
2018-05-03 15:33:32 -0400

    PM2 udpates.

--- scripts/check_server_status.sh
+++ scripts/check_server_status.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+PM2='/root/.npm-global/bin/pm2'
+if [ $($PM2 status | grep online | grep IquidusExplorer | wc -l) -ne 1 ]; then
+  echo "Script not running"
+  $PM2 delete IquidusExplorer
+  $PM2 start npm --name IquidusExplorer -- start
+  #$PM2 save
+  #$PM2 startup ubuntu
+fi
+
 if [ -f tmp/index.pid ]; then
   echo "file index.pid exists" >>/tmp/index-state.log
   ls -lt tmp >>/tmp/index-state.log
