7d3b761661b95fd7eff852ae67095a4b4cf31150
Aruljothi Parthiban <aruljothiparthiban@hotmail.com>
2018-08-07 09:13:07 +0530

    error logger

--- app.js
+++ app.js
@@ -122,6 +122,7 @@ app.use(function(err, req, res, next) {
         message : err.message,
         error : err
     };
+    console.log(error);
     if (env === 'production') {
         error.error = {};
     }
