e745c479f556514178adab5bf926feb89119c750
Aruljothi Parthiban <aruljothiparthiban@hotmail.com>
2018-08-29 18:35:56 +0530

    debug mongodb to fix topology error

--- helpers/db/connect.js
+++ helpers/db/connect.js
@@ -4,18 +4,35 @@ var settings = require('../../lib/settings');
 
 exports.connect = (cb) => {
     var dbString = 'mongodb://';
+    var options = {
+        useNewUrlParser : true
+    };
     if (settings.dbsettings.user && settings.dbsettings.password && !isPi()) {
         dbString += settings.dbsettings.user;
         dbString += ':' + settings.dbsettings.password + '@';
+
+        options = {
+            useNewUrlParser : true,
+            server : { 
+                socketOptions: { 
+                    keepAlive: 1, 
+                    connectTimeoutMS: 30000 
+                } 
+            },
+            replset: { 
+                socketOptions: { 
+                    keepAlive: 1, 
+                    connectTimeoutMS: 30000 
+                } 
+            }
+        };
     }
     dbString += settings.dbsettings.address;
     dbString += ':' + settings.dbsettings.port;
     dbString += '/' + settings.dbsettings.database;
     dbString += '?authSource=' + settings.dbsettings.database + '&w=1';
 
-    mongoose.connect(dbString, {
-        useNewUrlParser: true
-    }, function (err) {
+    mongoose.connect(dbString, options, function (err) {
         if (err) {
             console.log('Unable to connect to database: %s', dbString);
             console.log(err);
